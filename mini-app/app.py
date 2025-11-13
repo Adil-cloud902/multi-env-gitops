b3f1811bcd08c279c5747edaea91743d0e5a3d24
adil <adiladilel81@gmail.com>
2025-11-13 17:43:26 +0100

    Fix application contenu

--- mini-app/app.py
+++ mini-app/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Application Flask déployée en environnement PROD via GitLab CI/CD !"
+    return jsonify({
+        "message": "Hello from Flask App by Adil",
+        "version": os.getenv("APP_VERSION", "1.0.0"),
+        "environment": os.getenv("NODE_ENV", "development")
+    })
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
+    # écouter sur 0.0.0.0 pour Kubernetes
     app.run(host='0.0.0.0', port=5000)
